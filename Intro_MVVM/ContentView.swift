702ff5528bfc02e17c99286475c2e79a1c348712
Sonzzini <paulo.sonzzini@gmail.com>
2025-08-23 13:45:11 -0300

    Initial Commit

---
+++ Intro_MVVM/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Intro_MVVM
+//
+//  Created by Paulo Sonzzini Ribeiro de Souza on 23/08/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Intro_MVVM/Intro_MVVMApp.swift
@@ -0,0 +1,17 @@
+//
+//  Intro_MVVMApp.swift
+//  Intro_MVVM
+//
+//  Created by Paulo Sonzzini Ribeiro de Souza on 23/08/25.
+//
+
+import SwiftUI
+
+@main
+struct Intro_MVVMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
